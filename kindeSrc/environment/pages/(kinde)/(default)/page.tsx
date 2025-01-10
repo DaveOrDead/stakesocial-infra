2081532ce1cb637e050212f8b99440d1fadb9791
Dave Berner <davidajberner@gmail.com>
2025-01-10 13:28:04 +1100

    feat: add stake branding

--- kindeSrc/environment/pages/(kinde)/(default)/page.tsx
+++ kindeSrc/environment/pages/(kinde)/(default)/page.tsx
@@ -13,16 +13,22 @@ import {
   getLogoUrl,
   getSVGFavicon,
   setKindeDesignerCustomProperties,
+  getDarkModeLogoUrl,
 } from "../../utils/kindeInfra";
 
 const Layout = async ({ request, context }) => {
   return (
-    <html lang={request.locale.lang} dir={request.locale.isRtl ? "rtl" : "ltr"}>
+    <html
+      lang={request.locale.lang}
+      dir={request.locale.isRtl ? "rtl" : "ltr"}
+      data-kinde-theme="dark"
+    >
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="robots" content="noindex" />
         <meta name="csrf-token" content={getKindeCSRF()} />
+        <meta name="color-scheme" content="dark" />
         <title>{context.widget.content.page_title}</title>
 
         <link rel="icon" href={getSVGFavicon()} type="image/svg+xml" />
@@ -31,12 +37,12 @@ const Layout = async ({ request, context }) => {
         <style nonce={getKindeNonce()}>
           {`:root {
           ${setKindeDesignerCustomProperties({
-            baseBackgroundColor: "#fff",
-            baseLinkColor: "#230078",
-            buttonBorderRadius: "0.5rem",
-            primaryButtonBackgroundColor: "#230078",
+            baseBackgroundColor: "#010101",
+            baseLinkColor: "#ff1493",
+            buttonBorderRadius: "2em",
+            primaryButtonBackgroundColor: "#ff1493",
             primaryButtonColor: "#fff",
-            inputBorderRadius: "0.5rem",
+            inputBorderRadius: "0",
           })}}
           `}
         </style>
@@ -47,9 +53,6 @@ const Layout = async ({ request, context }) => {
                 --kinde-base-font-family: -apple-system, system-ui, BlinkMacSystemFont, Helvetica, Arial, Segoe UI, Roboto, sans-serif;
             }
 
-            [data-kinde-control-select-text]{
-                background-color: rgb(250, 250, 251);
-            }
             .c-container {
               padding: 1.5rem;
               display: grid;
@@ -59,16 +62,6 @@ const Layout = async ({ request, context }) => {
                 max-width: 400px;
                 width: 100%;
                 margin: 0px auto;
-            }
-            .c-footer {
-              border-top: 1px solid rgba(12, 0, 32, 0.08);
-              padding-block: 1.5rem;
-              display: flex;
-              justify-content: space-between;
-            }
-            .c-footer-links {
-                display: flex;
-                gap: 1.5rem;
             }
           `}
         </style>
@@ -80,27 +73,11 @@ const Layout = async ({ request, context }) => {
           </header>
           <main>
             <div class="c-widget">
-              <h2>{context.widget.content.heading} DEFAULT HARDCODED</h2>
+              <h2>{context.widget.content.heading}</h2>
               <p>{context.widget.content.description} </p>
               <div>{getKindeWidget()}</div>
             </div>
           </main>
-          <footer class="c-footer">
-            <p class="c-no-account-link">
-              No account? <a href="">Sign up for free</a>
-            </p>
-            <ul class="c-footer-links">
-              <li>
-                <a href="">Privacy</a>
-              </li>
-              <li>
-                <a href="">Terms</a>
-              </li>
-              <li>
-                <a href="">Get help</a>
-              </li>
-            </ul>
-          </footer>
         </div>
       </body>
     </html>
