b37db1739fae87679136e6da9a795ca23773a2f8
Dave Berner <davidajberner@gmail.com>
2025-01-10 15:30:11 +1100

    feat: remove children

--- kindeSrc/environment/pages/components/Layout.tsx
+++ kindeSrc/environment/pages/components/Layout.tsx
@@ -75,7 +75,7 @@ export const Layout = async ({ request, context, children }) => {
       </head>
       <body>
         <div id="root" data-roast-root="/admin" class="c-container">
-          {children}
+          Hello world
         </div>
       </body>
     </html>
