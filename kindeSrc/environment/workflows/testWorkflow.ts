9357c554295913f7520eb98de8164e2497e501a3
Dave Berner <davidajberner@gmail.com>
2025-05-27 13:04:13 +1000

    fix: binding

--- kindeSrc/environment/workflows/testWorkflow.ts
+++ kindeSrc/environment/workflows/testWorkflow.ts
@@ -6,14 +6,12 @@ export const workflowSettings = {
   },
   trigger: "user:plan_selection",
   bindings: {
-    kinde.planSelection: {}
-  }
+    "kinde.planSelection": {},
+  },
 };
 
 export default async function handlePasswordReset(event) {
   console.log("planSelection", event);
-  kinde.planSelection.denyAccess(
-    "Nah mate"
-  );
+  kinde.planSelection.denyAccess("Nah mate");
   console.log("done");
 }
