f0bb63884550097567869d02986128dd9c713c46
Dave Berner <davidajberner@gmail.com>
2025-06-25 20:59:42 +1000

    chore: test token caching

--- kindeSrc/environment/workflows/postAuthWorkflow.ts
+++ kindeSrc/environment/workflows/postAuthWorkflow.ts
@@ -1,13 +1,35 @@
-export const workflowSettings = {
+import {
+  createKindeAPI,
+  fetch,
+  getEnvironmentVariable,
+  WorkflowSettings,
+  WorkflowTrigger,
+  version,
+} from "@kinde/infrastructure";
+
+export const workflowSettings: WorkflowSettings = {
   id: "postAuthWF",
   name: "Post-Auth Workflow",
   failurePolicy: {
     action: "continue",
   },
   trigger: "user:post_authentication",
-  bindings: {},
+  bindings: {
+    "kinde.env": {},
+    "kinde.fetch": {},
+  },
 };
 
 export default async function Workflow(event) {
   console.log("postauth", event);
+
+  const kindeAPI = await createKindeAPI(event);
+
+  const { clientId } = event.context.application;
+  console.log({ version });
+  // Call Kinde applications properties API
+  const { data } = await kindeAPI.get({
+    endpoint: `applications/${clientId}/properties`,
+  });
+  const { appProperties } = data;
 }
