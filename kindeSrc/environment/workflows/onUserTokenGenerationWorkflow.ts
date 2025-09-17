d8dba26b385966248b4a89da138e377ac7a713d3
Dave <davidajberner@gmail.com>
2025-09-17 18:05:10 +1000

    Update onUserTokenGenerationWorkflow.ts

--- kindeSrc/environment/workflows/onUserTokenGenerationWorkflow.ts
+++ kindeSrc/environment/workflows/onUserTokenGenerationWorkflow.ts
@@ -12,19 +12,22 @@ export const workflowSettings = {
     "kinde.accessToken": {
       resetClaims: true,
     },
+    "kinde.ssoSession": {}
   },
 };
 
 export default async function handlePreMFA({request, context}) {
-    const res = await kinde.fetch("https://api.stakesocial.com/v1/get_sports", {
-      method: "GET",
-      responseFormat: "json",
-    });
-
-    console.log("stakeRes", res);
-    kinde.accessToken.setCustomClaim("sport", res.json.data[0].name);
-    console.log({ request, context });
-    kinde.idToken.setCustomClaim("sport", res.json.data[1].name);
-    kinde.accessToken.setCustomClaim("dave", "and_daniel_is_cool");
-    return "testing add user tokens claim";
+    // const res = await kinde.fetch("https://api.stakesocial.com/v1/get_sports", {
+    //   method: "GET",
+    //   responseFormat: "json",
+    // });
+  const policy = 'non_persistent';
+kinde.ssoSession.setPolicy(policy);
+  console.log('running');
+    // console.log("stakeRes", res);
+    // kinde.accessToken.setCustomClaim("sport", res.json.data[0].name);
+    // console.log({ request, context });
+    // kinde.idToken.setCustomClaim("sport", res.json.data[1].name);
+    // kinde.accessToken.setCustomClaim("dave", "and_daniel_is_cool");
+    // return "testing add user tokens claim";
 };
